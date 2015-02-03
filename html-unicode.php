595701a9b2b613381b35c85f7d3670ae97108f9b
Jens Lind <jens.lind@oddhill.se>
2015-02-03 12:00:56 +0100

    Added comments

--- html-unicode.php
+++ html-unicode.php
@@ -1,9 +1,30 @@
 <?php
+/**
+ * @file
+ * Convert html tags to unicode charachters. This is needed for example when you want to make sure a html tag is handled as ONE charachter.
+ * This code is a port from examples in the comments here: https://code.google.com/p/google-diff-match-patch/wiki/Plaintext
+ *
+ */
 
 class HtmlUnicode {
   private $htmlHash;
   private $currentHash;
 
+  function __construct() {
+    // Clear the hash table
+    $this->clearHash();
+  }
+
+  /**
+   * Generate a hash for a html tag and saves both to the hash table.
+   * They are saved so we can convert back to html again.
+   *
+   * @param string $tag
+   *  The html tag you want to generate a unicode char for.
+   *
+   * @return string
+   *  Returns the generated unicode char.
+   */
   private function pushHash($tag) {
     $this->htmlHash;
     $this->currentHash;
@@ -19,11 +40,23 @@ class HtmlUnicode {
     return $this->htmlHash[$tag];
   }
 
+  /**
+   * Clear the hash table.
+   */
   public function clearHash() {
     $this->htmlHash = NULL;
     $this->currentHash = 10240;
   }
 
+  /**
+   * Replace all html tags with unicode chars.
+   *
+   * @param string $html
+   *  String that contains html
+   *
+   * @return string
+   *  Returns the plain text without html tags
+   */
   public function html2plain($html) {
 
     $document = new DOMDocument();
@@ -41,6 +74,15 @@ class HtmlUnicode {
     return $html;
   }
 
+  /**
+   * Replace unicode chars back to html tags.
+   *
+   * @param string $plain
+   *  String that contains unicode chars an NO html.
+   *
+   * @return string
+   *  Returns html text without the unicode chars.
+   */
   public function plain2html($plain) {
     foreach ($this->htmlHash as $tag => $uni) {
       $plain = str_replace($uni, $tag, $plain);
